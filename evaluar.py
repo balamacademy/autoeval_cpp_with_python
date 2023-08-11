7e70beae9ab4258f757a6be43a7c9f96b299c433
Ek-Adan <adan@latailabs.com>
2023-08-10 23:29:50 -0600

    multi test

--- evaluar.py
+++ evaluar.py
@@ -10,42 +10,52 @@ elif platform == "win32":
 
 dir_path = r'.'
 res = []
+def read_exercises(conf_path='./conf_eval.json'):
+    conf_file = open(conf_path)
+    exercises = json.load(conf_file)
+    return exercises
 
-conf_file = open('conf_eval.json')
-ejercicios = json.load(conf_file)
-
-for file_path in os.listdir(dir_path):
-    if os.path.isfile(os.path.join(dir_path, file_path)):
-        if file_path[-3:] == "cpp":
-            res.append(file_path)
-
-for program in res:
-    conf = ejercicios.get(program, None)
-    if conf is not None:
+def eval_exercises(exercises):
+    for conf in exercises:
+        exercise = exercises[conf]
+        print(exercise)
+        filename = exercise['filename']
         result = subprocess.run(
-            ["g++", program], capture_output=True, text=True
+            ["g++", filename], capture_output=True, text=True
         )    
         if result.stderr:
-            conf["salida_obtenida"] = result.stderr
+            exercise["obtained_output"] = result.stderr
         else:
-            p = subprocess.Popen(f"./a.{EXT}", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            
-            if 'inputs' in conf:
-                for v in conf['inputs']:
-                    p.stdin.write( v.encode() + b'\n' )
-            
-            outs, errs  = p.communicate()
-            conf["salida_obtenida"] = outs.decode('utf-8')
-            if errs is not None:
-                conf["errors"] = errs.decode('utf-8')          
+            for test in exercise['tests']:
+                p = subprocess.Popen(f"./a.{EXT}", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                
+                if 'inputs' in test:
+                    for v in test['inputs']:
+                        p.stdin.write( v.encode() + b'\n' )
+                
+                outs, errs  = p.communicate()
+                test["obtained_output"] = outs.decode('utf-8')
+                if errs is not None:
+                    test["errors"] = errs.decode('utf-8') 
 
-puntuacion = 0
-n_ejercicios = 0
-for k in ejercicios:
-    n_ejercicios += 1
-    print(k)
-    print(ejercicios[k]['salida_obtenida'], ejercicios[k]['salida_esperada'], ejercicios[k]['salida_obtenida'] == ejercicios[k]['salida_esperada'])
-    puntuacion += 1 if ejercicios[k]['salida_obtenida'] == ejercicios[k]['salida_esperada'] else 0
+if __name__ == "__main__":
+    exercises = read_exercises()
+    eval_exercises(exercises)
 
-print(ejercicios)
-print(f'{puntuacion}/{n_ejercicios} = {puntuacion/n_ejercicios*100}')
+    puntuacion = 0
+    n_ejercicios = 0
+    for k in exercises:
+        exercise = exercises[k]        
+        print(k)
+        n_ejercicios += 1
+        n_test = 0
+        puntuacion_test = 0
+        for test in exercise['tests']:            
+            print(test['obtained_output'], test['expected_output'], test['obtained_output'] == test['expected_output'])
+            puntuacion_test += 1 if test['obtained_output'] == test['expected_output'] else 0
+            n_test += 1 
+        exercise['score'] = puntuacion_test/n_test
+        puntuacion += exercise['score']
+
+    print(exercises)
+    print(f'{puntuacion}/{n_ejercicios} = {puntuacion/n_ejercicios*100}')
